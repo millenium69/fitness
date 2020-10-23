e5e2c1bc0c8c78909488a56c4d52d37aeb7b4e92
MilleniumWenn <wenns>
2020-10-23 20:41:12 +0300

    My simple project

---
+++ Fitness/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fitness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
